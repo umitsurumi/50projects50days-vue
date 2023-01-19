57bf7e156b33f554814294fa1667048bfbc4b9a3
umitsurumi <39759460+umitsurumi@users.noreply.github.com>
2023-01-19 14:11:31 +0800

    updated

--- src/components/day9-sound-board/Day9Demo.vue
+++ src/components/day9-sound-board/Day9Demo.vue
@@ -1,70 +1,91 @@
 <template>
-  <div class="py-10 px-4 w-full bg-sky-800 text-white">
-    <form class="mx-auto px-10 py-5 max-w-sm bg-sky-900 rounded-xl shadow-md">
-      <h3 class="mb-8 text-center text-3xl">Please Login</h3>
-      <div
-        class="relative mb-8"
-        :class="{ day8active: formStatus === `${key}` || formInfo[key] !== '' }"
-        v-for="key in Object.keys(formInfo)">
-        <input
-          :type="`${key}`"
-          :id="`day8${key}`"
-          required
-          class="py-4 w-full border-b-2 border-white bg-transparent outline-none"
-          v-model="formInfo[key]"
-          @focus="formStatus = `${key}`"
-          @blur="formStatus = ''" />
-        <label
-          :for="`day8${key}`"
-          class="absolute top-4 left-0">
-          <span
-            class="transition-all inline-block"
-            v-for="(i, index) in `${key[0].toUpperCase() + key.slice(1)}`"
-            :style="{ 'transition-delay': `${index * 50}ms` }"
-            >{{ i }}</span
-          >
-        </label>
-      </div>
-      <button class="mb-6 py-2 w-full text-black bg-sky-200 rounded">
-        Login
-      </button>
-      <p>
-        Don't have an account?<a
-          href="#"
-          class="text-blue-400"
-          >Register</a
-        >
-      </p>
-    </form>
+  <div class="w-full">
+    <button
+      class="fixed h-8 px-2 text bg-slate-500/50 shadow rounded"
+      @click="switchTone === 1 ? switchTone++ : switchTone--">
+      {{ switchTone === 1 ? "平假名" : "片假名" }}
+    </button>
+    <table class="mx-auto w-1/2">
+      <tr v-for="i in 11">
+        <td v-for="item in divideTone(i * 5)">
+          <div
+            class="flex my-2 p-1 w-16 h-16 items-end rounded-lg cursor-pointer hover:shadow-lg dark:shadow-slate-300/60"
+            @click="playTone(item[1])">
+            <p class="w-10 h-10 text-4xl self-start font-semibold">
+              {{ item[switchTone] }}
+            </p>
+            <p class="relative -left-1">{{ item[0] }}</p>
+            <audio
+              :id="`${item[1]}`"
+              :src="`./src/assets/audio/learn50/${item[0]}.mp3`"></audio>
+          </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue";
-const formStatus = ref("");
-
-interface FormInfo {
-  email: string;
-  password: string;
-  [key: string]: string;
+const fiftyTone = reactive([
+  ["a", "あ", "ア"],
+  ["i", "い", "イ"],
+  ["u", "う", "ウ"],
+  ["e", "え", "エ"],
+  ["o", "お", "オ"],
+  ["ka", "か", "カ"],
+  ["ki", "き", "キ"],
+  ["ku", "く", "ク"],
+  ["ke", "け", "ケ"],
+  ["ko", "こ", "コ"],
+  ["sa", "さ", "サ"],
+  ["si", "し", "シ"],
+  ["su", "す", "ス"],
+  ["se", "せ", "セ"],
+  ["so", "そ", "ソ"],
+  ["ta", "た", "タ"],
+  ["ti", "ち", "チ"],
+  ["tu", "つ", "ツ"],
+  ["te", "て", "テ"],
+  ["to", "と", "ト"],
+  ["na", "な", "ナ"],
+  ["ni", "に", "ニ"],
+  ["nu", "ぬ", "ヌ"],
+  ["ne", "ね", "ネ"],
+  ["no", "の", "ノ"],
+  ["ha", "は", "ハ"],
+  ["hi", "ひ", "ヒ"],
+  ["hu", "ふ", "フ"],
+  ["he", "へ", "ヘ"],
+  ["ho", "ほ", "ホ"],
+  ["ma", "ま", "マ"],
+  ["mi", "み", "ミ"],
+  ["mu", "む", "ム"],
+  ["me", "め", "メ"],
+  ["mo", "も", "モ"],
+  ["ya", "や", "ヤ"],
+  ["i", "い", "イ"],
+  ["yu", "ゆ", "ユ"],
+  ["e", "え", "エ"],
+  ["yo", "よ", "ヨ"],
+  ["ra", "ら", "ラ"],
+  ["ri", "り", "リ"],
+  ["ru", "る", "ル"],
+  ["re", "れ", "レ"],
+  ["ro", "ろ", "ロ"],
+  ["wa", "わ", "ワ"],
+  ["i", "い", "イ"],
+  ["u", "う", "ウ"],
+  ["e", "え", "エ"],
+  ["wo", "を", "ヲ"],
+  ["n", "ん", "ン"],
+]);
+const switchTone = ref(1);
+function divideTone(n: number) {
+  return fiftyTone.slice(n - 5, n);
 }
-const formInfo: FormInfo = reactive({
-  email: "",
-  password: "",
-});
+function playTone(id: string) {
+  const audio: HTMLAudioElement | null = document.querySelector(`#${id}`);
+  audio?.play();
+}
 </script>
-
-<style lang="scss" scoped>
-// active的样式
-.day8active {
-  input {
-    @apply border-blue-400;
-  }
-  label {
-    @apply text-blue-400;
-    span {
-      @apply -translate-y-8;
-    }
-  }
-}
-</style>
