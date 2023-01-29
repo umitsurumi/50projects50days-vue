0aa3d02e79b4b25a4130a7a9d2575c191dfd9c4b
umitsurumi <39759460+umitsurumi@users.noreply.github.com>
2023-01-29 22:17:15 +0800

    day22

--- src/components/day20-button-ripple-effect/Day20Demo.vue
+++ src/components/day20-button-ripple-effect/Day20Demo.vue
@@ -1,7 +1,41 @@
 <template>
-  <div></div>
+  <div class="flex-center h-[40vh]">
+    <button
+      class="relative w-32 h-16 overflow-hidden text-lg text-slate-100 bg-purple-700 rounded-lg shadow-md"
+      @click="rippleEffect">
+      <div
+        class="absolute -translate-x-1/2 -translate-y-1/2 w-28 h-28 bg-white rounded-full"
+        :style="{ top: `${rippleTop}px`, left: `${rippleLeft}px` }"
+        v-show="showRipple"></div>
+      Button
+    </button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const rippleTop = ref(0);
+const rippleLeft = ref(0);
+const showRipple = ref(false);
+function rippleEffect(e: MouseEvent) {
+  const btn = e.target as HTMLElement;
+  rippleTop.value = e.pageY - btn.offsetTop;
+  rippleLeft.value = e.pageX - btn.offsetLeft;
+  showRipple.value = true;
+  setTimeout(() => {
+    showRipple.value = false;
+  }, 500);
+}
+</script>
 
-<style scoped></style>
+<style scoped>
+button div {
+  animation: rippleScale 0.5s ease-in;
+}
+@keyframes rippleScale {
+  to {
+    transform: translate(-50%, -50%) scale(3);
+    opacity: 0;
+  }
+}
+</style>
--- src/components/day22-drawing-app/Day22Demo.vue
+++ src/components/day22-drawing-app/Day22Demo.vue
@@ -1,7 +1,126 @@
 <template>
-  <div></div>
+  <div class="w-full bg-white border border-black">
+    <div
+      class="flex items-center justify-between w-full h-10 text-black bg-blue-900">
+      <div class="flex items-center">
+        <input
+          :class="brush.width > 1 ? 'cursor-pointer' : 'cursor-not-allowed'"
+          type="button"
+          value="-"
+          @click="brush.width > 1 ? brush.width-- : false" />
+        <input
+          type="text"
+          v-model="brush.width" />
+        <input
+          :class="brush.width < 30 ? 'cursor-pointer' : 'cursor-not-allowed'"
+          type="button"
+          value="+"
+          @click="brush.width < 30 ? brush.width++ : false" />
+        <input
+          type="color"
+          v-model="brush.color" />
+      </div>
+      <div class="mx-1 bg-slate-200 cursor-pointer">
+        <XMarked
+          @click="
+            ctx !== null
+              ? ctx.clearRect(
+                  0,
+                  0,
+                  canvasObj.canvas.width,
+                  canvasObj.canvas.height
+                )
+              : false
+          " />
+      </div>
+    </div>
+    <canvas
+      class="w-full h-[50vh] min-h-[300px] cursor-cell"
+      id="day22canvas"
+      :width="canvasObj.width"
+      :height="canvasObj.height"
+      @mousedown="drawStart"
+      @mouseup="drawEnd"
+      @mousemove="draw"></canvas>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, nextTick } from "vue";
+import { XMarked } from "../icons/SvgIcons.vue";
+let ctx: CanvasRenderingContext2D | null = null;
+interface canvasObj {
+  canvas: any;
+  width: number;
+  height: number;
+}
+const canvasObj: canvasObj = reactive({
+  canvas: null,
+  width: 0,
+  height: 0,
+});
+const brush = reactive({
+  isDrawing: false,
+  width: 1,
+  color: "#000000",
+  x: 0,
+  y: 0,
+});
 
-<style scoped></style>
+function drawStart(e: MouseEvent) {
+  brush.isDrawing = true;
+  brush.x = e.offsetX;
+  brush.y = e.offsetY;
+  console.log(`x:${brush.x},y:${brush.y}`);
+  console.log(canvasObj.canvas.width);
+}
+function drawEnd(e: MouseEvent) {
+  brush.isDrawing = false;
+  brush.x = 0;
+  brush.y = 0;
+}
+
+function draw(e: MouseEvent) {
+  if (brush.isDrawing) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    console.log(`x2:${x2},y2:${y2}`);
+    drawCircle(x2, y2);
+    drawLine(brush.x, brush.y, x2, y2);
+    brush.x = x2;
+    brush.y = y2;
+  }
+  function drawCircle(x: number, y: number) {
+    if (ctx !== null) {
+      ctx.beginPath();
+      ctx.arc(x, y, brush.width, 0, Math.PI * 2);
+      ctx.fillStyle = brush.color;
+      ctx.fill();
+    }
+  }
+  function drawLine(x1: number, y1: number, x2: number, y2: number) {
+    if (ctx !== null) {
+      ctx.beginPath();
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.strokeStyle = brush.color;
+      ctx.lineWidth = brush.width * 2;
+      ctx.stroke();
+    }
+  }
+}
+
+nextTick(() => {
+  canvasObj.canvas = document.getElementById("day22canvas");
+  ctx = canvasObj.canvas.getContext("2d");
+  console.log(canvasObj.canvas);
+  canvasObj.width = canvasObj.canvas.offsetWidth;
+  canvasObj.height = canvasObj.canvas.offsetHeight;
+});
+</script>
+
+<style scoped>
+input {
+  @apply mx-1 w-6 h-6 text-center  bg-slate-200;
+}
+</style>
